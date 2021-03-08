c0f4ee4dd9c4f720c265684f892714b33c948b1a
jamesncox <jamesncox@yahoo.com>
2021-03-08 14:18:08 -0600

    Update layout.module.css and add .profile.jpg to first-post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Image from 'next/image'
 import Layout from '../../components/layout'
 
 export default function FirstPost() {
@@ -14,6 +15,12 @@ export default function FirstPost() {
                 <a>Back to home</a>
             </Link>
         </h2>
+        <Image
+            src="/images/profile.jpg" // Route of the image file
+            height={144} // Desired size with correct aspect ratio
+            width={144} // Desired size with correct aspect ratio
+            alt="Your Name"
+        />
     </Layout>
   )
 }
