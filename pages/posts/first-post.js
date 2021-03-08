b7fda48a142b2f211cf9e5adb096356b60165b5b
jamesncox <jamesncox@yahoo.com>
2021-03-08 14:35:14 -0600

    Update index.js with my content/image

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -15,12 +15,6 @@ export default function FirstPost() {
                 <a>Back to home</a>
             </Link>
         </h2>
-        <Image
-            src="/images/profile.jpg" // Route of the image file
-            height={144} // Desired size with correct aspect ratio
-            width={144} // Desired size with correct aspect ratio
-            alt="Your Name"
-        />
     </Layout>
   )
 }
