ec4bb1b4c47b66a8c3227461ba5e6ad1d5617597
jamesncox <jamesncox@yahoo.com>
2021-03-09 14:31:30 -0600

    Create pages/404.js custom404()

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+export default function Custom404() {
+    return (
+        <div style={{textAlign: "center", marginTop: "40vh", backgroundColor: "black", color: "white"}}>
+            <h1>404 - Page Not Found</h1>
+            <h2>YOU DONE DID IT NOW</h2>
+        </div>
+    )
+}
