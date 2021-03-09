7d303db82862f077df5dc7086962e63956663b98
jamesncox <jamesncox@yahoo.com>
2021-03-09 14:18:02 -0600

    Import utilStyles to posts/[id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,8 @@
+import { getAllPostIds, getPostData } from '../../lib/posts'
 import Layout from '../../components/layout'
 import Head from 'next/head'
 import Date from '../../components/date'
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id)
