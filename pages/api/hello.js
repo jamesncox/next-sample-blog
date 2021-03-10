f8439b2e600aa18fb1fe2a4d4042e7197a86447a
jamesncox <jamesncox@yahoo.com>
2021-03-10 13:29:36 -0600

    Update api/hello.js with more JSON

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,8 @@
 export default function handler(req, res) {
-    res.status(200).json({ text: 'Hello' })
+    res.status(200).json({
+        id: 1,
+        type: 'Greeting',
+        text: 'Hello',
+        language: 'English'
+    })
   }
