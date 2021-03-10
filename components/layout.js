c18e21d1898eb798ac3b3d67d50d899064254965
jamesncox <jamesncox@yahoo.com>
2021-03-10 14:39:06 -0600

    Update layout.js name emoji to wolf

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'James Cox 🦊'
+const name = 'James Cox 🐺'
 export const siteTitle = `James' Blog`
 
 export default function Layout({ children, home }) {
